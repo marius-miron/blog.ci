82ed3bf73fe9caa4c709512d0bc81ee3c334a03e
Marius Miron <marius.costel.miron@gmail.com>
2014-07-30 22:31:45 +0300

    adding login controller

---
+++ application/controllers/login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * login controller
+ * 
+ */
+class Login extends CI_Controller {
+
+   /**
+    * index action
+    */
+    public function index() {
+        $this->load->view('login/index');
+    }
+
+}
