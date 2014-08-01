2bfb1e8befc036b1b61845c35a546dd621e5016b
Marius Miron <marius.costel.miron@gmail.com>
2014-08-01 11:33:34 +0300

    adding  errors display

---
+++ application/views/register/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <style>
+            body {
+                padding-top: 40px;
+                padding-bottom: 40px;
+                background-color: #eee;
+            }
+
+            .form-signin {
+                max-width: 330px;
+                padding: 15px;
+                margin: 0 auto;
+            }
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
+            .form-signin .checkbox {
+                font-weight: normal;
+            }
+            .form-signin .form-control {
+                position: relative;
+                height: auto;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+                padding: 10px;
+                font-size: 16px;
+            }
+            .form-signin .form-control:focus {
+                z-index: 2;
+            }
+            .form-signin input[type="email"] {
+                margin-bottom: -1px;
+                border-bottom-right-radius: 0;
+                border-bottom-left-radius: 0;
+            }
+            .form-signin input[type="password"] {
+                margin-bottom: 10px;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <?php echo validation_errors(); ?>
+            <form role="form" action="<?php echo base_url('register/indexPost'); ?>" class="form-signin" method="POST">
+                <h2 class="form-signin-heading">Please register with us</h2>
+                <input type="text" autofocus="" required="" name="username" placeholder="Username" class="form-control">
+                <input type="password" required="" name="password" placeholder="Password" class="form-control">
+                <input type="password" required="" name="password2" placeholder="Confirm password" class="form-control">
+                                
+                <button type="submit" class="btn btn-lg btn-success btn-block ">Create account</button>
+            </form>
+
+        </div>
+
+        <p class="footer"></p>
+    </body>
+</body>
+</html>
