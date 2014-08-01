e6068cbc06e7eff3d4b10a9a01c302542c078afc
Marius Miron <marius.costel.miron@gmail.com>
2014-08-01 21:45:26 +0300

    setting the validation errors displaying red background and above the form

--- application/views/register/index.php
+++ application/views/register/index.php
@@ -59,11 +59,11 @@
             
             <form role="form" action="<?php echo base_url('register/indexPost'); ?>" class="form-signin" method="POST">
                 <h2 class="form-signin-heading">Please register with us</h2>
-                <div class="alert alert-danger" role="alert"> <?php echo validation_errors(); ?></div>
-                <input type="text" autofocus="" required="" name="username" placeholder="Username" class="form-control">
-                <input type="password" required="" name="password" placeholder="Password" class="form-control">
-                <input type="password" required="" name="password2" placeholder="Confirm password" class="form-control">
-                                
+                <div class="alert alert-danger"><?php echo validation_errors(); ?></div>
+                <input type="text" autofocus="" required="" name="username" placeholder="Username" class="form-control has-success has-error">
+                <input type="password" required="" name="password" placeholder="Password" class="form-control has-success has-error">
+                <input type="password" required="" name="password2" placeholder="Confirm password" class="form-control has-success has-error">
+                               
                 <button type="submit" class="btn btn-lg btn-success btn-block ">Create account</button>
             </form>
 
