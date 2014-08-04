4ea443d3cf2d8b7dc32ea15faf583172a41c80e6
Marius Miron <marius.costel.miron@gmail.com>
2014-08-04 12:10:26 +0300

    added validateUser action

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -12,4 +12,29 @@ class Login extends CI_Controller {
         $this->load->view('login/index');
     }
 
+    /**
+     *  validateUser action
+     */
+    
+    public function validateUser() {
+
+        $this->load->library('form_validation');
+        $this->load->model('User');
+        $query = $this->User->validate();
+
+        if ($query) {
+            //VALID login
+            $data = array(
+                'username' => $this->input->post('username'),
+                'is_logged_in' => true
+            );
+
+            $this->session->set_userdata($data);
+            redirect('dashboard/index');
+        } else {
+            //INVALID login
+            $this->session->set_flashdata('message', 'Username or/and password incorrect.');
+            redirect('login');
+        }
+    }
 }
