de1a257faf7b7b22764fe181a54a2e29d5f04675
Marius Miron <marius.costel.miron@gmail.com>
2014-08-04 15:03:33 +0300

    added newPost action

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,11 +1,24 @@
 <?php
 
+/**
+ *  Dashboard controller
+ */
 class Dashboard extends CI_Controller {
 
+    /**
+     * index action
+     */
     public function index() {
 
         $this->load->view('dashboard/index');
     }
      
-
+    /**
+     * newPost action
+     */
+    public function newPost(){
+        
+        $this->load->view('dashboard/newPost');
+        
+    }
 }
