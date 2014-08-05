eb3b5414f35dc002f2fbd64be7048e284ed48cf4
Marius Miron <marius.costel.miron@gmail.com>
2014-08-06 00:24:08 +0300

    added base url for action of the submit button

--- application/views/dashboard/newPost.php
+++ application/views/dashboard/newPost.php
@@ -44,12 +44,12 @@
         
         <div class="container">
             <div class="row">
-                <form role="form" action="" class="form-signin" method="POST">
+                <form role="form" action="<?php echo base_url('dashboard/indexPost'); ?>" class="form-signin" method="POST">
                     <h2 class="form-signin-heading">Please leave us a post !</h2>
                     </br>
                     <input type="text" autofocus="" required="" name="subject" placeholder="Subject here" class="form-control">
                     </br>
-                    <textarea required="" name="textarea" placeholder="Your comment goes here" class="form-control" rows="6"></textarea>
+                    <textarea required="" name="message" placeholder="Your comment goes here" class="form-control" rows="6"></textarea>
                     </br></br>
                     <button type="submit" class="btn btn-lg btn-primary btn-block">Save</button>
                     
