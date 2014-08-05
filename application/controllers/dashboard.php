3a2f1572d07de940f73161be003271adb4539851
Marius Miron <marius.costel.miron@gmail.com>
2014-08-05 23:51:24 +0300

    added indexPost action

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -12,13 +12,45 @@ class Dashboard extends CI_Controller {
 
         $this->load->view('dashboard/index');
     }
-     
+
     /**
      * newPost action
      */
     public function newPost(){
-        
+
         $this->load->view('dashboard/newPost');
-        
+    
     }
+
+    /**
+     * indexPost action
+     */
+    public function indexPost() {
+
+        $user_id = $this->session->userdata('user_id');
+
+        $this->load->library('form_validation');
+
+        //--- field name, error message, validation rules
+        $this->form_validation->set_rules('subject', 'Subject here', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('message', 'Your comment goes here', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+
+            // Display error message
+            $this->session->set_flashdata('flashError', 'This is an error message.');
+            redirect('dashboard/newPost');
+        } else {
+            $this->load->model('Post');
+            $this->Post->setSubject($this->input->post('subject'));
+            $this->Post->setMessage($this->input->post('message'));
+            $this->Post->setUserId($user_id);
+            $this->Post->insertIntoDb();
+
+            // Display success message
+            $this->session->set_flashdata('flashSuccess', 'Your new post has been successfully saved !');
+            redirect('dashboard/index');
+        }
+    }
+
 }
