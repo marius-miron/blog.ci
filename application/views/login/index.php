9df37d8db721c26c505dd52f13ec2dfb53091b5e
Marius Miron <marius.costel.miron@gmail.com>
2014-08-06 22:35:05 +0300

    setting flash messages for successful/ unsuccessful logout

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -62,6 +62,18 @@
                     <?php if ($message = $this->session->flashdata('message')): ?>
                             <div class="alert alert-danger"><?php echo $message; ?></div>
                     <?php endif;?>
+                     <div class="row placeholders">
+                        <?php if ($success = $this->session->flashdata('flashSuccess')): ?>
+                            <div class="alert alert-success alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span></button><?php echo $success; ?></div>
+                        <?php endif;?>
+                         <?php if ($error = $this->session->flashdata('flashError')): ?>
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span></button><?php echo $error; ?></div>
+                        <?php endif; ?>
+                    </div>       
                     <input type="text" autofocus="" required="" name="username" placeholder="Username" class="form-control">
                     <input type="password" required="" name="password" placeholder="Password" class="form-control">
                     <div class="checkbox">
