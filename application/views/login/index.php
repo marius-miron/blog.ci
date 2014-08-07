c309c6c3520f8e2b203a7795a68f3a92b4d67d54
Marius Miron <marius.costel.miron@gmail.com>
2014-08-07 16:52:21 +0300

    improved layout

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,59 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Login</title>
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://code.jquery.com/jquery.js"></script>
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-        <style>
-            body {
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: #eee;
-            }
 
-            .form-signin {
-                max-width: 330px;
-                padding: 15px;
-                margin: 0 auto;
-            }
-            .form-signin .form-signin-heading,
-            .form-signin .checkbox {
-                margin-bottom: 10px;
-            }
-            .form-signin .checkbox {
-                font-weight: normal;
-            }
-            .form-signin .form-control {
-                position: relative;
-                height: auto;
-                -webkit-box-sizing: border-box;
-                -moz-box-sizing: border-box;
-                box-sizing: border-box;
-                padding: 10px;
-                font-size: 16px;
-            }
-            .form-signin .form-control:focus {
-                z-index: 2;
-            }
-            .form-signin input[type="email"] {
-                margin-bottom: -1px;
-                border-bottom-right-radius: 0;
-                border-bottom-left-radius: 0;
-            }
-            .form-signin input[type="password"] {
-                margin-bottom: 10px;
-                border-top-left-radius: 0;
-                border-top-right-radius: 0;
-            }
-        </style>
-    </head>
+<?php $this->load->view('includes/header'); ?>
+
     <body>
         <div class="container">
             <div class="row">
@@ -87,7 +34,4 @@
             </div>
         </div>
 
-        <p class="footer"></p>
-    </body>
-</body>
-</html>
+<?php $this->load->view('includes/footer'); ?>
