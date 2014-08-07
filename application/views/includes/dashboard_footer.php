e26ff5c352fe01dd98d852bb0b4332b69ff17f37
Marius Miron <marius.costel.miron@gmail.com>
2014-08-07 16:55:36 +0300

    created dashboard_footer view for improved layout of dashboard

---
+++ application/views/includes/dashboard_footer.php
@@ -0,0 +1,12 @@
+ 
+
+        <p class="footer"></p>
+
+<!-- Bootstrap core JavaScript
+            ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+            <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+            <script src="http://getbootstrap.com/assets/js/docs.min.js"></script>
+    </body>
+</html>
